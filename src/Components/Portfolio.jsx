7ec9888eecd9db1435a72cefd3c8dbd4cfa5d138
Pranjal500 <singhpranjal822@gmail.com>
2024-07-03 16:19:59 +0530

    Commited

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -21,7 +21,7 @@ import React from "react";
  */
 import image from "../images/git1.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "My github profile";
 
 /**
  * Project list
